bc3df9db99b99acdf8d7aa1e2272c380ee5c9d1c
Mohamed Gaber <donn@efabless.com>
2024-02-26 11:49:53 +0200

    Updates to account for default of `SYNTH_ABC_BUFFERING` changing (#21)
    * lower utilization to fix congestion
    
    * restore SYNTH_ABC_BUFFERING for some designs that performed worse when variable was truned off
    
    ---------
    
    Co-authored-by: Kareem Farid <kareefardi@users.noreply.github.com>

--- xtea/config.tcl
+++ xtea/config.tcl
@@ -7,6 +7,8 @@ set ::env(SDC_FILE) "$::env(DESIGN_DIR)/src/xtea.sdc"
 set ::env(CLOCK_PORT) "clock"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
+set ::env(SYNTH_ABC_BUFFERING) 1
+
 set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
