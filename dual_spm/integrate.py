6cf274d77312275fb23a21e680daed50e1ec8c9a
Mohamed Gaber <donn@efabless.com>
2024-03-13 13:43:07 +0200

    More new tests for macro black-boxing (#23)
    * Reorganized `aes_user_project_wrapper/config.json`
    * New design `dual_spm`, full one-script integration example for the fastest test set
    * New design `cell_inverter`, Synthesis-only flow for one hand-instantiated cell for both sky130A and gf180mcuD

---
+++ dual_spm/integrate.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# Copyright 2024 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import click
+
+from openlane.flows.classic import Classic
+from openlane.common import ScopedFile
+from openlane.config import Macro
+from openlane.logging import options
+
+__dir__ = os.path.dirname(__file__)
+
+
+@click.command(context_settings={"ignore_unknown_options": True})
+@click.option("--pdk-root", type=click.Path(dir_okay=True, file_okay=False))
+@click.option("--run-tag", type=click.Path(dir_okay=True, file_okay=False))
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
+def main(
+    pdk_root,
+    run_tag,
+    args,
+):
+    options.set_condensed_mode(True)
+
+    spm_v = ScopedFile(
+        contents="""
+        module spm #(parameter bits=32) (
+            input clk,
+            input rst,
+            input x,
+            input[bits-1: 0] a,
+            output y
+        );
+            wire[bits: 0] y_chain;
+            assign y_chain[0] = 0;
+            assign y = y_chain[bits];
+
+            wire[bits-1:0] a_flip;
+            generate 
+                for (genvar i = 0; i < bits; i = i + 1) begin : flip_block
+                    assign a_flip[i] = a[bits - i - 1];
+                end 
+            endgenerate
+
+            DelayedSerialAdder dsa[bits-1:0](
+                .clk(clk),
+                .rst(rst),
+                .x(x),
+                .a(a_flip),
+                .y_in(y_chain[bits-1:0]),
+                .y_out(y_chain[bits:1])
+            );
+
+        endmodule
+
+        module DelayedSerialAdder(
+            input clk,
+            input rst,
+            input x,
+            input a,
+            input y_in,
+            output reg y_out
+        );
+            reg lastCarry;
+            wire lastCarry_next;
+            wire y_out_next;
+
+            wire g = x & a;
+            assign {lastCarry_next, y_out_next} = g + y_in + lastCarry;
+
+            always @ (posedge clk or negedge rst) begin
+                if (!rst) begin
+                    lastCarry <= 1'b0;
+                    y_out <= 1'b0;
+                end else begin
+                    lastCarry <= lastCarry_next;
+                    y_out <= y_out_next;
+                end
+            end
+        endmodule
+    """
+    )
+
+    macro_flow = Classic(
+        {
+            "DESIGN_NAME": "spm",
+            "CLOCK_PORT": "clk",
+            "VERILOG_FILES": [spm_v],
+            "FP_PDN_CORE_RING": True,
+            "RUN_KLAYOUT_DRC": False,
+            "FP_PDN_CORE_RING_VWIDTH": 3.1,
+            "FP_PDN_CORE_RING_HWIDTH": 3.1,
+            "FP_PDN_CORE_RING_VOFFSET": 12.45,
+            "FP_PDN_CORE_RING_HOFFSET": 12.45,
+            "FP_PDN_CORE_RING_VSPACING": 1.7,
+            "FP_PDN_CORE_RING_HSPACING": 1.7,
+            "FP_PDN_VPITCH": 80,
+            "FP_PDN_HPITCH": 80,
+            "FP_PDN_VWIDTH": 3.2,
+            "FP_PDN_HWIDTH": 3.2,
+            "FP_PDN_VSPACING": 3.2,
+            "FP_PDN_HSPACING": 3.2,
+            "FP_MACRO_HORIZONTAL_HALO": 20,
+            "FP_MACRO_VERTICAL_HALO": 20,
+        },
+        design_dir=__dir__,
+        pdk="sky130A",
+        pdk_root=pdk_root,
+    )
+
+    macro_state = macro_flow.start(tag=os.path.join(run_tag, "macro"))
+    # macro_state = State.loads(open("runs/spm/54-checker-lvs/state_out.json").read())
+
+    spm = Macro.from_state(macro_state)
+    spm.instantiate("spm_1", (150, 150))
+    spm.instantiate("spm_2", (300, 300))
+
+    integration_v = ScopedFile(
+        contents="""
+        module dual_spm(
+        `ifdef USE_POWER_PINS
+            inout VPWR,
+            inout VGND,
+        `endif
+            input clk,
+            input rstn,
+            input x1,
+            input x2,
+            input[31:0] a1,
+            input[31:0] a2,
+            output y1,
+            output y2
+        );
+            spm spm_1 (
+            `ifdef USE_POWER_PINS
+                .VPWR(VPWR),
+                .VGND(VGND),
+            `endif
+                .clk(clk),
+                .rst(rstn),
+                .x(x1),
+                .a(a1),
+                .y(y1)
+            );
+            spm spm_2 (
+            `ifdef USE_POWER_PINS
+                .VPWR(VPWR),
+                .VGND(VGND),
+            `endif
+                .clk(clk),
+                .rst(rstn),
+                .x(x2),
+                .a(a2),
+                .y(y2)
+            );
+        endmodule    
+        """
+    )
+
+    integration_flow = Classic(
+        {
+            "DESIGN_NAME": "dual_spm",
+            "CLOCK_PORT": "clk",
+            "VERILOG_FILES": [integration_v],
+            "MACROS": {"spm": spm},
+            "FP_SIZING": "absolute",
+            "DIE_AREA": [0, 0, 500, 500],
+            "FP_PDN_CORE_RING_VWIDTH": 3.1,
+            "FP_PDN_CORE_RING_HWIDTH": 3.1,
+            "FP_PDN_CORE_RING_VOFFSET": 12.45,
+            "FP_PDN_CORE_RING_HOFFSET": 12.45,
+            "FP_PDN_CORE_RING_VSPACING": 1.7,
+            "FP_PDN_CORE_RING_HSPACING": 1.7,
+            "FP_PDN_VWIDTH": 3.2,
+            "FP_PDN_HWIDTH": 3.2,
+            "FP_PDN_VSPACING": 3.2,
+            "FP_PDN_HSPACING": 3.2,
+            "FP_PDN_VPITCH": 40,
+            "FP_PDN_HPITCH": 40,
+            "FP_MACRO_HORIZONTAL_HALO": 20,
+            "FP_MACRO_VERTICAL_HALO": 20,
+            "FP_PDN_CHECK_NODES": False,
+            "RUN_IRDROP_REPORT": False,
+            "RUN_KLAYOUT_DRC": False,
+            "RUN_KLAYOUT_STREAMOUT": False,
+            "RUN_KLAYOUT_XOR": False,
+        },
+        design_dir=__dir__,
+        pdk="sky130A",
+        pdk_root=pdk_root,
+    )
+
+    integration_flow.start(tag=os.path.join(run_tag, "integration"))
+
+
+if __name__ == "__main__":
+    main()
